3913e961aa1d75d3c05c7d3c2ab693e0a8a60971
Yossan Rahmadi <yossansr06@gmail.com>
2022-02-20 18:25:12 +0800

    Initial Commit

---
+++ TableViewUpdates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewUpdates
+//
+//  Created by Yossan Rahmadi on 20/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
